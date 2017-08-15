7f97b879d2f7f123aba8f611ee728cdcd0480124
qwertpi <qwertpi@users.noreply.github.com>
2017-08-15 13:13:17 +0100

    Update main.py to print fruit names

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-import msvcrt
-import time
-
-keystate = 'HIGH'
+import sys, termios, tty, os, time
+ 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+ 
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+ 
+button_delay = 0.2
+ 
 while True:
-  if msvcrt.kbhit():
-    msvcrt.getch()
-    keystate = 'LOW'
-  else:
-    keystate = 'HIGH'
-  # you may want to put a time.sleep() call here
-  # to avoid eating up CPU
-  time.sleep(0.1)
-  print(keystate)
+    char = getch()
+ 
+    if (char == "w"):
+        print("Bannana")
+        time.sleep(button_delay)
+ 
+    elif (char == "s"):
+        print("Lime")
+        time.sleep(button_delay)
+ 
+    elif (char == "d"):
+        print("Orange")
+        time.sleep(button_delay)
+ 
+    elif (char == "a"):
+        print("Lemon")
+        time.sleep(button_delay)
