0be2d1fb3269b148fc69df0acdabe6b400db6542
qwertpi <qwertpi@users.noreply.github.com>
2017-08-15 13:29:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
-import sys, termios, tty, os, time
- 
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
- 
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
- 
-button_delay = 0.2
- 
-while True:
-    char = getch()
- 
-    if (char == "w"):
-        print("Bannana")
-        time.sleep(button_delay)
- 
-    elif (char == "s"):
-        print("Lime")
-        time.sleep(button_delay)
- 
-    elif (char == "d"):
-        print("Orange")
-        time.sleep(button_delay)
- 
-    elif (char == "a"):
-        print("Lemon")
-        time.sleep(button_delay)
+import msvrct
+print("Press s or n to continue")
+input_char=msvcrt.getch()
+if input_char.upper()=='W':
+  print("Strawberry")
+if input_char.upper()=='A':
+  print("Bannana")
+  if input_char.upper()=='S':
+  print("Lemon")
+  if input_char.upper()=='D':
+  print("Melon")
