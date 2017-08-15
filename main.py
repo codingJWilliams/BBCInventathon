38ce65446d07051e6f28b7ceb4e136c538f6b8a6
Jay Williams <course@jaywilliams.me>
2017-08-15 11:09:52 +0100

    Actual code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# Test
+import msvcrt
+import time
+
+keystate = 'HIGH'
+while True:
+  if msvcrt.kbhit():
+    msvcrt.getch()
+    keystate = 'LOW'
+  else:
+    keystate = 'HIGH'
+  # you may want to put a time.sleep() call here
+  # to avoid eating up CPU
+  time.sleep(0.1)
+  print(keystate)
