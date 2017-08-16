fc9ae1a7d37e623ae1ebd573be107091fb22b8f8
qwertpi <qwertpi@users.noreply.github.com>
2017-08-16 10:13:48 +0100

    Added scoring

--- main.py
+++ main.py
@@ -20,82 +20,99 @@ orange='1'
 bannana='2'
 lemon='3'
 lime='4'
+#set score to 0
+score=0
 #save current cpu time in the start_time variable
 start_time=time.time()
 #while current time minus start tiem is less than or equal to 60
 while time.time()-start_time<=60:
   #save a randomly generated number between 0 and 12 to the number variable
   number=randint(0,12)
-  #if number equal 0 then
+  #if number equals 0 then
   if number==0:
     #while input_char variable isn't equal to the lemon variable
     while input_char!=lemon:
       #show_image
       show_image("/assets/bannana-lemon.png")
-    #close image by finding pid of process sheoing image and then killing it
+    score=score+1
+    #close image by finding pid of process showing image and then killing it
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==1:
     while input_char!=orange:
       show_image("/assets/bannana-orange.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==2:
     while input_char!=lime:
       show_image("/assets/bannana-lime.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==3:
     while input_char!=bannana:
       show_image("/assets/lemon-bannana.png")
+    score=score+1
+    for proc in psutil.process_iter():
+      if proc.name() == "display":
+        proc.kill()
   elif number==4:
     while input_char!=orange:
       show_image("/assets/lemon-orange.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==5:
     while input_char!=lime:
       show_image("/assets/lemon-lime.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==6:
     while input_char!=bannana:
       show_image("/assets/orange-bannana.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==7:
     while input_char!=lemon:
       show_image("/assets/orange-lemon.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==8:
     while input_char!=lime:
       show_image("/assets/orange-lime.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==9:
     while input_char!=bannana:
       show_image("/assets/lime-bannana.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==10:
     while input_char!=lemon:
       show_image("/assets/lime-lemon.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
   elif number==11:
     while input_char!=orange:
       show_image("/assets/lime-orange.png")
+    score=score+1
     for proc in psutil.process_iter():
       if proc.name() == "display":
         proc.kill()
