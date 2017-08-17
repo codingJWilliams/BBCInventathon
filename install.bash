639ff25d71800044e9372af434c894c434864e45
qwertpi <qwertpi@users.noreply.github.com>
2017-08-17 11:01:25 +0100

    Delete install.bash

--- install.bash
+++
@@ -1,8 +0,0 @@
-echo "Downlaoding files"
-git clone https://github.com/codingJWilliams/BBCInventathon.git
-cd BBCInventathon
-rm -f todo.md
-echo "You now need to go to makeymakey.com/remap/ and follow the instructions"
-echo "Once you have done that connect the fruit to the makey makey then hit enter"
-read enter
-python3 setup.py
